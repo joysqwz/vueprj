da9b9861533578197ccffafaed4c44989e079e0a
joy-n <joysqwzweb@gmail.com>
2025-05-18 23:55:29 +0300

    fix rec

--- client/src/helpers/http.helper.js
+++ client/src/helpers/http.helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'https://vueprj-sfmeied.ru/api'
+export const API_URL = 'https://vueprj-sfmeied.ru//api'
 
 const $api = axios.create({
   baseURL: API_URL,
@@ -11,13 +11,15 @@ $api.interceptors.response.use(
   (res) => res,
   async (error) => {
     const originalRequest = error.config
-
     if (error.response?.status === 401 && !originalRequest._isRetry) {
       originalRequest._isRetry = true
       try {
         await axios.get(`${API_URL}/auth/refresh`, { withCredentials: true })
         return $api.request(originalRequest)
       } catch (err) {
+        if (originalRequest.url.includes('/check-auth')) {
+          return Promise.reject(err)
+        }
         window.location.href = '/login'
         return Promise.reject(err)
       }
